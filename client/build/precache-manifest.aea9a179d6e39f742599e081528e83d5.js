2b3b531c0a994240024189dfc4d12dd254220e19
Joven <joflip@gmail.com>
2019-04-25 09:09:39 -0400

    Initial node_modules commit

---
+++ client/build/precache-manifest.aea9a179d6e39f742599e081528e83d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa0d37897ac358e8711f26a11a97d928",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5ee75568bf80f7d71f7e",
+    "url": "/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "d29615c2a5e7e8058675",
+    "url": "/static/js/2.cd17db81.chunk.js"
+  },
+  {
+    "revision": "5ee75568bf80f7d71f7e",
+    "url": "/static/js/main.0c0361a4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
